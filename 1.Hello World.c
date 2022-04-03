2ea2abb706bee7c331b2abd141cce72f975cacd3
gabrielhtg <gabrielhutagalung970@gmail.com>
2022-04-03 22:18:40 +0700

    baru

---
+++ 1.Hello World.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main () {
+    char s[200];
+    
+    scanf("%[^\n]%*c", s);
+    printf("Hello, World! \n%s", s);
+    
+    return 0;
+}
